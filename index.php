ff96a3079af4cee3f6bb6dad6d3cb7a81c0e92d2
Ozan <anarcheist@gmail.com>
2014-11-01 16:19:36 +0200

    İndex
    Açıklamalar içerisinde mevcuttur.

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+// Her dosyaya fonksiyonları çekmek zorundayız.
+require_once('fonksiyonlar.php');
+
+
+// Değer gönderelim
+	//--> Bir fonksiyon çıktısı almış olalım
+	//--> atayıp bastıralım
+
+$verimiz = "Burası index sayfasıdır";	
+$yakala->ata('anahtar', $verimiz);
+
+// Anasayfa.tpl sayfamızı çağıralım
+$yakala->ekran('anasayfa');
+
+?>
